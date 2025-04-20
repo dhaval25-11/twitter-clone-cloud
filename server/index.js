23c6261aeab45cb153356eb6dd5969efdc9b3897
dhaval25-11 <dhavalsolanki2443@gmail.com>
2025-04-20 17:55:08 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API Running');
+});
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(process.env.PORT || 5000, () => console.log('Server running')))
+  .catch(err => console.error(err));
