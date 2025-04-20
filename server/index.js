b26a85270cfa977c433fef7df85c6c9868452431
dhaval25-11 <dhavalsolanki2443@gmail.com>
2025-04-20 22:17:25 +0530

    Added test-db route to check MongoDB

--- server/index.js
+++ server/index.js
@@ -12,6 +12,17 @@ app.get('/', (req, res) => {
   res.send('API Running');
 });
 
+app.get('/test-db', async (req, res) => {
+  try {
+    const Test = mongoose.model('Test', new mongoose.Schema({ msg: String }));
+    await Test.create({ msg: 'MongoDB is working!' });
+    const docs = await Test.find();
+    res.json(docs);
+  } catch (error) {
+    res.status(500).json({ error: 'MongoDB connection failed', details: error.message });
+  }
+});
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(process.env.PORT || 5000, () => console.log('Server running')))
   .catch(err => console.error(err));
